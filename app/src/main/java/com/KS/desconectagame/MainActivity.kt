3cb110e5ed97c7c2596becb30c15d1b6b45f41b5
Kalleb <kallebjs07@gmail.com>
2025-04-07 10:09:51 -0300

    Tela home criada, botao de login sem validacao

--- app/src/main/java/com/KS/desconectagame/MainActivity.kt
+++ app/src/main/java/com/KS/desconectagame/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.KS.desconectagame
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+    val Logar: Button = findViewById(R.id.bt_logar)
+        Logar.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
